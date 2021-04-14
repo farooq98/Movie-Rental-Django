637f57b6c4d55594e9966a46233de6d24b8aaad7
Farooq Hidayat <farooq@Farooq-MacBook-Pro.local>
2021-04-14 18:23:06 +0500

    first commit

---
+++ videostore/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from . import models
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = models.Movies
+        fields = ['movie_name', 'movie_year', 'movie_genere', 'price']
+        widgets = {
+            'movie_name': forms.TextInput(attrs={'class':'form-control'}),
+            'movie_year': forms.TextInput(attrs={'class':'form-control'}),
+            'movie_genere': forms.TextInput(attrs={'class':'form-control'}),
+            'price': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+
+class MemberForm(forms.ModelForm):
+    class Meta:
+        model = models.Members
+        fields = ['member_name', 'member_address']
+        widgets = {
+            'member_name': forms.TextInput(attrs={'class':'form-control'}),
+            'member_address': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class MovieRentalForm(forms.ModelForm):
+
+    return_date = forms.DateInput()
+    
+    class Meta:
+        model = models.Movie_Rental
+        fields = ['member_id', 'movie_id', 'return_date']
+        widgets = {
+            'member_id': forms.Select(attrs={'class':'form-control', 'readonly':'readonly'}),
+            'movie_id': forms.Select(attrs={'class':'form-control', 'readonly':'readonly'}),
+            'return_date': DateInput(),
+        }
+
+class MemberNameForm(forms.Form):
+
+    Choices = tuple([(data['id'], data['member_name']) for data in models.Members.objects.all().values('member_name', 'id')])
+    member_name = forms.ChoiceField(choices = Choices, widget=forms.Select(attrs={'class':'form-control'}))
+    
+
